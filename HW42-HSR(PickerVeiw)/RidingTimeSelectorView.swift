d67c11bed1dd3abe3d3f3678d21bc5d8bb4615ef
Daweihao84 <dwsamurai84@gmail.com>
2024-01-22 13:30:52 +0800

    20240122: 1. Add RidingTimeSelectorView as custom View.

---
+++ HW42-HSR(PickerVeiw)/RidingTimeSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  RidingTimeSelectorView.swift
+//  HW42-HSR(PickerVeiw)
+//
+//  Created by Dawei Hao on 2024/1/22.
+//
+
+import UIKit
+
+class RidingTimeSelectorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

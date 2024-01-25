a921476178562874eccfae1f856ee6d49bbe9c47
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-01-26 02:01:45 +0800

    20240126: 1. Create MoreTableView's model. 2. Add SafariServies in MoreTableViewController.

---
+++ HW42-HSR(PickerVeiw)/Model/MoreTableView+Model.swift
@@ -0,0 +1,60 @@
+//
+//  MoreTableView+Model.swift
+//  HW42-HSR(PickerVeiw)
+//
+//  Created by Da-wei Hao on 2024/1/26.
+//
+
+import Foundation
+
+struct More {
+    let icon : String
+    let title: String
+    let url  : String
+}
+
+let sectionOne = [
+More(icon:  "",
+     title: "高鐵時刻表",
+     url:   ""),
+More(icon:  "",
+     title: "票價資訊",
+     url:   ""),
+More(icon:  "",
+     title: "查詢訂位代號",
+     url:   "")
+]
+
+let sectionTwo = [
+    More(icon:  "",
+         title: "交易注意事項",
+         url:   ""),
+    More(icon:  "",
+         title: "旅客運送契約",
+         url:   "https://www.thsrc.com.tw/event/regulations/ThsrcPassengerTransportationContract.pdf"),
+    More(icon:  "",
+         title: "使用導覽",
+         url:   "")
+]
+
+let sectionThree = [
+    More(icon:  "",
+         title: "關於 T Express",
+         url:   "https://www.thsrc.com.tw/ArticleContent/4d262503-84bc-4963-a58e-4ca1a6453ad3"),
+    More(icon:  "",
+         title: "無障礙服務",
+         url:   "https://www.thsrc.com.tw/ArticleContent/dba4ecd0-5ef0-45d2-aa64-bcecdf7d5fde?breadcrumbs=17b156fd-3e99-4d48-a63f-6dbc77823af3"),
+    More(icon:  "",
+         title: "前往高鐵網站",
+         url:   "https://www.thsrc.com.tw"),
+    More(icon:  "",
+         title: "前往台灣高鐵App",
+         url:   "https://apps.apple.com/tw/app/%E5%8F%B0%E7%81%A3%E9%AB%98%E9%90%B5/id1266626655?l=en-GB"),
+    More(icon:  "",
+         title: "前往高鐵 TLife 網站",
+         url:   "https://tlife.thsrc.com.tw/tw"),
+    More(icon:  "",
+         title: "高鐵運輸服務碳足跡",
+         url:   "https://www.thsrc.com.tw/t/carbonFootprint#stations"),
+]
+

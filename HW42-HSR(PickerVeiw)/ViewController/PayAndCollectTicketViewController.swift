3a0327564c2cfccda9b9f7132a068b7421fe625d
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-02-01 01:56:58 +0800

    Add the test for showing nonReservedView.

--- HW42-HSR(PickerVeiw)/ViewController/PayAndCollectTicketViewController.swift
+++ HW42-HSR(PickerVeiw)/ViewController/PayAndCollectTicketViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class PayAndCollectTicketViewController: UIViewController {
 
+    let backgroundView: UIView = UIView()
+    let nonReservedView: NonReservedView = NonReservedView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigationBar ()
         self.view.backgroundColor = Colors.brightGray
+        configureBackgroundView ()
     }
     
     func setupNavigationBar () {
@@ -40,6 +45,34 @@ class PayAndCollectTicketViewController: UIViewController {
         
         // Set up navigationItem's rightBarButton
         self.navigationItem.rightBarButtonItems = [accountBarButton, fixedSpace, speakerBarButton]
+    }
+    
+    func configureBackgroundView () {
+        
+        backgroundView.backgroundColor = Colors.brightGray
+        backgroundView.addSubview(nonReservedView)
+        
+        
+        backgroundView.layer.borderWidth = 1
+        backgroundView.layer.borderColor = Colors.black.cgColor
+        
+        backgroundView.heightAnchor.constraint(equalToConstant: 555).isActive = true
+        
+        view.addSubview(backgroundView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        nonReservedView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nonReservedView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 10),
+            nonReservedView.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor)
+        
+        ])
+        
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
     
     // MARK: - UIButton Action:

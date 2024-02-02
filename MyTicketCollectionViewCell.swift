4324354f8715b9093bd6f2317f716447bf4570e1
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-02-03 02:02:44 +0800

    Add MyTicketCollectionViewCell.

---
+++ MyTicketCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TicketCollectionViewCell.swift
+//  HW42-HSR(PickerVeiw)
+//
+//  Created by Da-wei Hao on 2024/2/3.
+//
+
+import UIKit
+
+class MyTicketCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+

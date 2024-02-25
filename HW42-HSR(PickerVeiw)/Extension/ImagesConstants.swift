04a80464b11419fa07a17f71a80d17e6fe900aaa
Daweihao84 <dwsamurai84@gmail.com>
2024-02-25 19:42:03 +0800

    Add data.

--- HW42-HSR(PickerVeiw)/Extension/ImagesConstants.swift
+++ HW42-HSR(PickerVeiw)/Extension/ImagesConstants.swift
@@ -9,56 +9,60 @@ import UIKit
 
 struct Images {
     // hsr icon
-    static let hsrImage           = UIImage(named: "hsrLogo")!
+    static let hsrImage                = UIImage(named: "hsrLogo")!
     
     // AccountCenter Infomation cell use:
-    static let folder             = UIImage(systemName: "folder")!
-    static let listBullet         = UIImage(systemName: "list.bullet.rectangle")!
-    static let checkmark          = UIImage(systemName: "checkmark.circle")!
-    static let personfill         = UIImage(systemName: "person.2.fill")!
-    static let personCircle       = UIImage(systemName: "person.circle")!
+    static let folder                  = UIImage(systemName: "folder")!
+    static let listBullet              = UIImage(systemName: "list.bullet.rectangle")!
+    static let checkmark               = UIImage(systemName: "checkmark.circle")!
+    static let personfill              = UIImage(systemName: "person.2.fill")!
+    static let personCircle            = UIImage(systemName: "person.circle")!
     
     // AccountCenter Second information cell use:
-    static let horizontalSlider  = UIImage(systemName: "slider.horizontal.3")!
-    static let globeIcon         = UIImage(systemName: "globe.europe.africa.fill")!
-    static let bell              = UIImage(systemName: "bell")!
-    static let speakerWaveBubble = UIImage(systemName: "speaker.wave.2.bubble")!
-    
+    static let horizontalSlider        = UIImage(systemName: "slider.horizontal.3")!
+    static let globeIcon               = UIImage(systemName: "globe.europe.africa.fill")!
+    static let bell                    = UIImage(systemName: "bell")!
+    static let speakerWaveBubble       = UIImage(systemName: "speaker.wave.2.bubble")!
+         
     // serviceSelectionCell use.
-    static let calendar          = UIImage(systemName: "calendar")!
-    static let tram              = UIImage(systemName: "tram.fill")!
-    static let carseat           = UIImage(systemName: "carseat.right")!
-    
+    static let calendar                = UIImage(systemName: "calendar")!
+    static let tram                    = UIImage(systemName: "tram.fill")!
+    static let carseat                 = UIImage(systemName: "carseat.right")!
+         
     // UIBarButtonItem:
-    static let speakerWave       = UIImage(systemName:"speaker.wave.3.fill")
-    static let arrow             = UIImage(systemName: "arrowtriangle.forward.fill")!
-    
+    static let speakerWave             = UIImage(systemName:"speaker.wave.3.fill")
+    static let arrow                   = UIImage(systemName: "arrowtriangle.forward.fill")!
+         
     // MoreTableView cell use.
-    static let clock             = UIImage(systemName: "clock")!
-    static let dollarSign        = UIImage(systemName: "dollarsign.circle")!
-    static let magnifyingglass   = UIImage(systemName: "text.magnifyingglass")!
-    static let docPlain          = UIImage(systemName: "doc.plaintext")!
-    static let doc               = UIImage(systemName: "doc.text")!
-    static let infomation        = UIImage(systemName: "info.circle")!
-    static let questionMark      = UIImage(systemName: "questionmark.circle")!
-    static let wheelChair        = UIImage(systemName: "figure.roll")!
-    static let arrowForward      = UIImage(systemName: "ipad.and.arrow.forward")!
+    static let clock                   = UIImage(systemName: "clock")!
+    static let dollarSign              = UIImage(systemName: "dollarsign.circle")!
+    static let magnifyingglass         = UIImage(systemName: "text.magnifyingglass")!
+    static let docPlain                = UIImage(systemName: "doc.plaintext")!
+    static let doc                     = UIImage(systemName: "doc.text")!
+    static let infomation              = UIImage(systemName: "info.circle")!
+    static let questionMark            = UIImage(systemName: "questionmark.circle")!
+    static let wheelChair              = UIImage(systemName: "figure.roll")!
+    static let arrowForward            = UIImage(systemName: "ipad.and.arrow.forward")!
+         
+         
+    static let more                    = UIImage(systemName: "ellipsis")!
+    static let seatsIconForTabBar      = UIImage(systemName: "carseat.left.massage")!
+    static let ticket                  = UIImage(systemName: "ticket")!
+    static let payAndLoad              = UIImage(systemName: "creditcard")!
+    static let loadReservation         = UIImage(systemName: "square.and.arrow.down")!
+        
+    static let bluetoothIcon           = UIImage(named: "bluetooth-Icon")!
+    static let circle                  = UIImage(systemName: "circle")!
+    static let hsrLogo                 = UIImage(named: "HSRIcon")!
+    static let tifeLogo                = UIImage(named: "TLifeIcon")!
+    static let co2Logo                 = UIImage(named: "Co2Icon")!
+             
+    static let forwardImage            = UIImage(systemName: "chevron.forward")!
+    static let backwardImage           = UIImage(systemName: "chevron.backward")!
+    static let nonReservationImage     = UIImage(named: "nonReservationImage")!
     
-    
-    static let more               = UIImage(systemName: "ellipsis")!
-    static let seatsIconForTabBar = UIImage(systemName: "carseat.left.massage")!
-    static let ticket             = UIImage(systemName: "ticket")!
-    static let payAndLoad         = UIImage(systemName: "creditcard")!
-    static let loadReservation    = UIImage(systemName: "square.and.arrow.down")!
-    
-    
-    static let bluetoothIcon  = UIImage(named: "bluetooth-Icon")!
-    static let circle         = UIImage(systemName: "circle")!
-    static let hsrLogo        = UIImage(named: "HSRIcon")!
-    static let tifeLogo       = UIImage(named: "TLifeIcon")!
-    static let co2Logo        = UIImage(named: "Co2Icon")!
-    
-    static let forwardImage   = UIImage(systemName: "chevron.forward")!
-    static let backwardImage  = UIImage(systemName: "chevron.backward")!
-    static let nonReservationImage = UIImage(named: "nonReservationImage")!
+    // CreditCardOfferView:
+    static let rectangleImage          = UIImage(systemName: "rectangle")!
+    static let checkmarkRectangleImage = UIImage(systemName: "checkmark.rectangle.fill")!
+    static let asterisk                = UIImage(systemName: "asterisk")!
 }

b81eb6bdd3c1508c38480a7e0bb946a3900f5674
Daweihao84 <dwsamurai84@gmail.com>
2024-03-02 23:04:36 +0800

    Set up squareButton for click count.

--- SquareButton.swift
+++ SquareButton.swift
@@ -9,18 +9,41 @@ import UIKit
 
 class SquareButton: UIButton {
     
+    // Property to hold the click count
+    private var clickCount = 0
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        setupButton()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        
-        configurationUpdateHandler = { button in
-                var config = button.configuration
-            config?.background.image = button.isHighlighted ? Images.square : Images.checkmarkSquare
-                button.configuration = config
+        setupButton()
+    }
+    
+    private func setupButton() {
+        // Add target for touch up inside action
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        // Initial button image
+        updateSquareButtonImage()
+    }
+    
+    @objc private func buttonTapped() {
+        // Increment click count
+        clickCount += 1
+        // Update button image based on click count
+        updateSquareButtonImage ()
+    }
+    
+    private func updateSquareButtonImage() {
+        // Determine the button image based on click count
+        // This is a simplified example; you can customize the logic as needed
+
+        if clickCount.isMultiple(of: 2) {
+            self.setImage(Images.square, for: .normal)
+        } else {
+            self.setImage(Images.checkmarkSquare, for: .normal)
         }
     }
 }
